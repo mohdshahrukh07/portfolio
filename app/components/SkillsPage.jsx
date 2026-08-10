6aabc3bbeaf3f722090341ec214fcbdb2e2002e8
Mohd Shahrukh <144126195+mohdshahrukh07@users.noreply.github.com>
2026-08-10 18:02:55 +0530

    skills

--- app/components/SkillsPage.jsx
+++ app/components/SkillsPage.jsx
@@ -94,16 +94,18 @@ export default function EducationPage() {
 
     ]
     return (
-        <div className="w-full h-auto flex justify-center items-center  bg-[#25262f] p-2 py-5 pt-10 pb-10">
+        <div className="w-full h-auto flex justify-center items-center  bg-[#25262f] p-2 py-15">
 
-            <div className=" w-full max-w-7xl h-auto flex justify-center items-start py-2 mt-4">
-                <div className="w-[45%] h-full px-5">
+            <div className=" w-full max-w-7xl h-auto flex justify-center items-start py-2 mt-4
+                max-sm:flex-col max-sm:items-center">
+                <div className="w-[45%] h-full px-5 pb-10 max-sm:w-full">
                     <h3 className="text-xl text-[#80db66] tracking-[0.25em] font-medium">My Skills</h3>
                     <h1 className="text-6xl text-white font-medium py-4">Growing Over Times</h1>
                     <p className="text-[#9098bb] mt-4 text-xl font-normal">
                         Sed ut perspiciatis unde omnis iste natus Sed ut perspiciatis unde omnis iste natus kobita tumi sopno charini hoye khbor nio na sit voluptatem accusantium dolore.</p>
                 </div>
-                <div className="w-[50vw] mx-auto grid grid-cols-4 gap-y-5 gap-x-6">
+                <div className="w-[50vw] mx-auto grid grid-cols-4 gap-y-5 gap-x-6 max-sm:w-full
+                max-sm:grid-cols-2 justify-items-center">
                                 {skills.map((skill) => (
                                     <div className="group h-40 w-40 [perspective:1000px]"
                                         key={skill.name}>
